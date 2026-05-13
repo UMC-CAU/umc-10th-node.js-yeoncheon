949fb4e8d00bc674b07be0e03c82ad95f564bc8f
안준형 <anjun0423@gmail.com>
2026-05-13 11:37:31 +0900

    5주차 과제 파일 삭제

--- 5week/src/modules/mission/Routes/mission.route.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { handleAddMission, handleChallengeMission } from "../controllers/mission.conroller";
-
-const missionRouter = Router();
-
-missionRouter.post("/stores/:storeId/missions", handleAddMission);
-missionRouter.post("/missions/:missionId/challenge", handleChallengeMission);
-
-export default missionRouter;
